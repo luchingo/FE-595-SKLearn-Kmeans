ad26d86d081d084899665422428612ae8f0caef8
luchingo <60360781+luchingo@users.noreply.github.com>
2020-04-28 11:35:18 +0200

    black

--- Assignment_4.py
+++ Assignment_4.py
@@ -37,7 +37,7 @@ print(
 
 iris = datasets.load_iris()
 
-iris_x = pd.DataFrame(iris.data, columns= iris.feature_names)
+iris_x = pd.DataFrame(iris.data, columns=iris.feature_names)
 iris_y = iris.target
 
 model = KMeans()
@@ -55,27 +55,39 @@ fin_model = KMeans(n_clusters=visualizer.elbow_value_)
 kmeans_fin = KMeans(n_clusters=visualizer.elbow_value_, random_state=10).fit(iris_x)
 
 plt.subplot(3, 2, 1)
-plt.scatter(iris_x.iloc[:, [0]], iris_x.iloc[:, [1]], c=kmeans_fin.labels_, cmap="rainbow")
-plt.title(''.join([iris_x.columns[0], " vs ", iris_x.columns[1]]))
+plt.scatter(
+    iris_x.iloc[:, [0]], iris_x.iloc[:, [1]], c=kmeans_fin.labels_, cmap="rainbow"
+)
+plt.title("".join([iris_x.columns[0], " vs ", iris_x.columns[1]]))
 
 plt.subplot(3, 2, 2)
-plt.scatter(iris_x.iloc[:, [0]], iris_x.iloc[:, [2]], c=kmeans_fin.labels_, cmap="rainbow")
-plt.title(''.join([iris_x.columns[0], " vs ", iris_x.columns[2]]))
+plt.scatter(
+    iris_x.iloc[:, [0]], iris_x.iloc[:, [2]], c=kmeans_fin.labels_, cmap="rainbow"
+)
+plt.title("".join([iris_x.columns[0], " vs ", iris_x.columns[2]]))
 
 plt.subplot(3, 2, 3)
-plt.scatter(iris_x.iloc[:, [0]], iris_x.iloc[:, [3]], c=kmeans_fin.labels_, cmap="rainbow")
-plt.title(''.join([iris_x.columns[0], " vs ", iris_x.columns[3]]))
+plt.scatter(
+    iris_x.iloc[:, [0]], iris_x.iloc[:, [3]], c=kmeans_fin.labels_, cmap="rainbow"
+)
+plt.title("".join([iris_x.columns[0], " vs ", iris_x.columns[3]]))
 
 plt.subplot(3, 2, 4)
-plt.scatter(iris_x.iloc[:, [1]], iris_x.iloc[:, [2]], c=kmeans_fin.labels_, cmap="rainbow")
-plt.title(''.join([iris_x.columns[1], " vs ", iris_x.columns[2]]))
+plt.scatter(
+    iris_x.iloc[:, [1]], iris_x.iloc[:, [2]], c=kmeans_fin.labels_, cmap="rainbow"
+)
+plt.title("".join([iris_x.columns[1], " vs ", iris_x.columns[2]]))
 
 plt.subplot(3, 2, 5)
-plt.scatter(iris_x.iloc[:, [1]], iris_x.iloc[:, [3]], c=kmeans_fin.labels_, cmap="rainbow")
-plt.title(''.join([iris_x.columns[1], " vs ", iris_x.columns[3]]))
+plt.scatter(
+    iris_x.iloc[:, [1]], iris_x.iloc[:, [3]], c=kmeans_fin.labels_, cmap="rainbow"
+)
+plt.title("".join([iris_x.columns[1], " vs ", iris_x.columns[3]]))
 
 plt.subplot(3, 2, 6)
-plt.scatter(iris_x.iloc[:, [2]], iris_x.iloc[:, [3]], c=kmeans_fin.labels_, cmap="rainbow")
-plt.title(''.join([iris_x.columns[2], " vs ", iris_x.columns[3]]))
+plt.scatter(
+    iris_x.iloc[:, [2]], iris_x.iloc[:, [3]], c=kmeans_fin.labels_, cmap="rainbow"
+)
+plt.title("".join([iris_x.columns[2], " vs ", iris_x.columns[3]]))
 
 plt.show()
